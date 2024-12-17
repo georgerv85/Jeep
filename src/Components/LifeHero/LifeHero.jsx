78a6c258c052b11d5b5570ad5809d6e8e4fcc2b4
George Arvanitis <giorgosea@gmail.com>
2024-12-17 23:39:19 +0300

    life

--- src/Components/LifeHero/LifeHero.jsx
+++ src/Components/LifeHero/LifeHero.jsx
@@ -11,20 +11,30 @@ function LifeHero() {
 
         <div className='lifeHero-container'>
 
-            <div className='rowLife'>
+            <div className='rowLife1'>
 
                 <h1>ΧΑΜΕΝΟΙ ΣΤΗΝ ΠΕΡΙΠΕΤΕΙΑ</h1>
 
             </div>
 
-            <div className='rowLife bottomLife'>
+            <div className='rowLife2 bottomLife'>
 
-                <div className='lifeBox'>1
-                    <img src={life1} alt="" />
+                <div className='lifeBox'>
+                    <img src={life1} alt="" className='lifePhoto'/>
                 </div>
 
-                <div className='lifeBox'>2
-                    {/* <img src={life1} alt="" /> */}
+                <div className='lifeBox'>
+                    <h1>ΕΚΕΙ ΠΟΥ ΤΟ ΠΝΕΥΜΑ ΤΗΣ JEEP® ΖΩΝΤΑΝΕΥΕΙ</h1>
+                    <p>Το Camp της Jeep® είναι το βασικό γεγονός για το Jeep® Owners Group (JOG) και έχει προσελκύσει, κατά τη διάρκεια των ετών, μαζική συμμετοχή από όλη την Ευρώπη.
+                    </p>
+                    <br />
+                    <p>
+                        Οι λάτρεις και οι ιδιοκτήτες της Jeep® ενώνονται για να επιβεβαιώσουν τον ειδικό δεσμό που υπάρχει μεταξύ της μάρκας Jeep® και της κοινότητάς της.
+                    </p>
+                    <br />
+                    <p>
+                    Ήταν πάντα μια μοναδική ευκαιρία να διασκεδάσετε και να ζήσετε αξέχαστες περιπέτειες.
+                    </p>
                 </div>
 
 
