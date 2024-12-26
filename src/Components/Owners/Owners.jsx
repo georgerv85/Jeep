6b73f4837eb8d189187e08b24d6fe3ce09ab67e8
George Arvanitis <giorgosea@gmail.com>
2024-12-26 18:45:44 +0300

    new

--- src/Components/Owners/Owners.jsx
+++ src/Components/Owners/Owners.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Owners.css'
-import owners1 from '../../assets/images/carousel2.jpg'
+
 
 function Owners() {
   return (
+
     <div className='owners-container container'>
 
         <div className='owners-text'>
@@ -13,12 +14,10 @@ function Owners() {
             <p>Τι λένε οι κάτοχοι των οχημάτων μας.</p>
             <hr></hr>
 
-
         </div>
-
-
-
+     
     </div>
+
   )
 }
 
