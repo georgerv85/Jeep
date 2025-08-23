0535e4138d5be728f8e7d192b2838c61a2562ff3
George Arvanitis <giorgosea@gmail.com>
2025-08-23 18:20:05 +0300

    new

--- src/Components/Footer2/Footer2.jsx
+++ src/Components/Footer2/Footer2.jsx
@@ -7,6 +7,7 @@ function programms() {
     <div className='footer-lower2'>
         <h3>ΕΞΥΠΗΡΕΤΗΣΗ ΠΕΛΑΤΩΝ JEEP® 800 11 500 80.</h3>
         <img src={footerIcon} alt="" />
+        <button>Web Developer: George Arvanitis</button>
         <p>Ευχαριστούμε για το ενδιαφέρον σας στη JEEP.</p>
         <p>Παρακαλούμε στείλτε το αίτημά σας για πληροφορίες ή το παράπονό σας ή την πρότασή σας στο info@jeep.gr </p> 
         <p>© 2019 FCA US LLC. Με επιφύλαξη όλων των δικαιωμάτων.Τα λογότυπα Chrysler, Jeep®, Dodge, Ram, Mopar® και το Λογότυπο με τα Πέντε Αστέρια είναι καταχωρημένα εμπορικά σήματα της εταιρείας FCA US LLC"</p>
